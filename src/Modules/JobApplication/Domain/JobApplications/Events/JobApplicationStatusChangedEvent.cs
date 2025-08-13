2b2693607bab66c2bee5c7347b2604fb749720ff
coolbeatz71 <sigmacool@gmail.com>
2025-08-13 20:44:10 +0200

    feat: create job application events and handlers

---
+++ src/Modules/JobApplication/Domain/JobApplications/Events/JobApplicationStatusChangedEvent.cs
@@ -0,0 +1,20 @@
+using Core.Domain;
+
+namespace JobApplication.Domain.JobApplications.Events;
+
+/// <summary>
+/// Represents a domain event that is raised when the status of a <see cref="JobApplicationEntity"/> changes.
+/// </summary>
+/// <param name="JobApplicationEntity">The job application entity whose status was updated.</param>
+/// <remarks>
+/// This event can be used to trigger updates in dependent systems, such as job or status history logs.
+/// </remarks>
+/// <example>
+/// <code>
+/// <![CDATA[
+/// application.Update(..., Status: ApplicationStatus.Shortlisted);
+/// var domainEvent = new JobApplicationStatusChangedEvent(application);
+/// ]]>
+/// </code>
+/// </example>
+public record JobApplicationStatusChangedEvent(JobApplicationEntity jobApplicationEntity): IDomainEvent;
