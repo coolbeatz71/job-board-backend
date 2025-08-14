3d545b72a777664994e4c78411cef5cf1dc49ee0
coolbeatz71 <sigmacool@gmail.com>
2025-08-14 20:14:35 +0200

    chore: register the authentication service to Main Program

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -38,18 +38,50 @@ builder.Services.AddAuthorization();
 
 builder.Services
     .AddJobModule(builder.Configuration)
+    .AddJobApplicationModule(builder.Configuration)
     .AddAuthenticationModule(builder.Configuration)
-    .AddJobApplicationModule(builder.Configuration);
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen(c =>
+        {
+            // Add JWT authentication to Swagger
+            c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                Name = "Authorization",
+                In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+                Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+
+            c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+            {
+                {
+                    new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                    {
+                        Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                        {
+                            Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        }
+    );
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
-app.MapCarter();
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseSerilogRequestLogging();
 app.UseExceptionHandler(_ => { });
 app.UseAuthentication();
 app.UseAuthorization();
+app.MapCarter();
 
 // Configure middleware extensions for job, application and authentication modules.
 app
