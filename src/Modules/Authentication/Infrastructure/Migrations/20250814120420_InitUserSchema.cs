0a1a0c34d13e1b2a92a70634add67b69a207a785
coolbeatz71 <sigmacool@gmail.com>
2025-08-14 14:08:18 +0200

    chore: create users and job application migrations

---
+++ src/Modules/Authentication/Infrastructure/Migrations/20250814120420_InitUserSchema.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Authentication.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitUserSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "authentication");
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                schema: "authentication",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    email = table.Column<string>(type: "text", maxLength: 50, nullable: false),
+                    password_hash = table.Column<string>(type: "text", nullable: false),
+                    role = table.Column<string>(type: "text", nullable: false, defaultValue: "JobSeeker"),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    created_by = table.Column<string>(type: "text", nullable: true),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    updated_by = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_email",
+                schema: "authentication",
+                table: "users",
+                column: "email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users",
+                schema: "authentication");
+        }
+    }
+}
